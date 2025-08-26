d604f3084425b5be0b287d20154e5c1ec8ba6853
Paulo Gusttavo Tognato <paulo.tognato@hotmail.com>
2025-08-26 20:24:20 -0300

    chore: adding api gw, handler e lambda

--- terraform/api_gateway.tf
+++ terraform/api_gateway.tf
@@ -9,16 +9,22 @@ resource "aws_api_gateway_resource" "users" {
   path_part   = "users"
 }
 
+resource "aws_api_gateway_resource" "register" {
+  rest_api_id = aws_api_gateway_rest_api.users_api.id
+  parent_id   = aws_api_gateway_resource.users.id
+  path_part   = "register"
+}
+
 resource "aws_api_gateway_method" "post_register" {
   rest_api_id   = aws_api_gateway_rest_api.users_api.id
-  resource_id   = aws_api_gateway_resource.users.id
+  resource_id   = aws_api_gateway_resource.register.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
 resource "aws_api_gateway_integration" "lambda_register" {
   rest_api_id             = aws_api_gateway_rest_api.users_api.id
-  resource_id             = aws_api_gateway_resource.users.id
+  resource_id             = aws_api_gateway_resource.register.id
   http_method             = aws_api_gateway_method.post_register.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
@@ -30,7 +36,7 @@ resource "aws_lambda_permission" "apigw_lambda" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.user_registration.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn    = "${aws_api_gateway_rest_api.users_api.execution_arn}/*/*"
+  source_arn    = "${aws_api_gateway_rest_api.users_api.execution_arn}/*/POST/users/register"
 }
 
 resource "aws_api_gateway_deployment" "users_api_deployment" {
