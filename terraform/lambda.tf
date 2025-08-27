d258480a4e0c34a808922d256674d02e75aa4f86
Paulo Gusttavo Tognato <paulo.tognato@hotmail.com>
2025-08-26 21:28:43 -0300

    chore: adding api gw, handler e lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,8 +4,8 @@ resource "aws_lambda_function" "user_registration" {
   runtime       = "java17"
   role          = aws_iam_role.lambda_exec_role.arn
 
-  filename         = "${path.module}/../build/quarkus-app/app/userregistration-1.0-SNAPSHOT.jar"
-  source_code_hash = filebase64sha256("${path.module}/../build/quarkus-app/app/userregistration-1.0-SNAPSHOT.jar")
+  filename         = "${path.module}/../build/quarkus-app/userregistration-1.0-SNAPSHOT-runner.jar"
+  source_code_hash = filebase64sha256("${path.module}/../build/quarkus-app/userregistration-1.0-SNAPSHOT-runner.jar")
 
   environment {
     variables = {
