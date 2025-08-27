5763e1965911fb850b76a80b99350af11258e3db
Paulo Gusttavo Tognato <paulo.tognato@hotmail.com>
2025-08-26 21:45:13 -0300

    chore: adding api gw, handler e lambda

--- src/main/java/com/example/adapter/lambda/Handler.java
+++ src/main/java/com/example/adapter/lambda/Handler.java
@@ -6,10 +6,12 @@ import com.example.adapter.http.api.dto.UserRequest;
 import com.example.adapter.http.api.mapper.UserMapper;
 import com.example.domain.application.UserService;
 import com.example.domain.entity.User;
+import jakarta.inject.Inject;
 
 public class Handler implements RequestHandler<UserRequest, User> {
 
-    private final UserService userService = new UserService();
+    @Inject
+    UserService userService;
 
     @Override
     public User handleRequest(UserRequest input, Context context) {
