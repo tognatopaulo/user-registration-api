97f2ff0be5d926acc18fa19ec135f05814aa5609
uiutuuz_deere <tognatopaulogusttavo@johndeere.com>
2025-09-05 17:12:42 -0300

    sp1 - Definição da estrutura inicial de dados para o DynamoDB

---
+++ src/main/java/com/example/infra/persistence/UserRepositoryDynamoDB.java
@@ -0,0 +1,58 @@
+package com.example.infra.persistence;
+
+import com.example.adapter.persistence.UserRepository;
+import com.example.domain.entity.User;
+import jakarta.enterprise.context.ApplicationScoped;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
+
+import java.util.*;
+
+@ApplicationScoped
+public class UserRepositoryDynamoDB implements UserRepository {
+
+    private static final String TABLE_NAME = "users";
+    private final DynamoDbClient dynamoDbClient;
+
+    public UserRepositoryDynamoDB(DynamoDbClient dynamoDbClient) {
+        this.dynamoDbClient = dynamoDbClient;
+    }
+
+    @Override
+    public void saveUser(User user) {
+        if (user.getUserId() == null || user.getUserId().isEmpty()) {
+            user.setUserId(UUID.randomUUID().toString());
+        }
+
+        Map<String, AttributeValue> item = new HashMap<>();
+        item.put("userId", AttributeValue.fromS(String.valueOf(user.getUserId())));
+        item.put("userName", AttributeValue.fromS(user.getUserName()));
+        item.put("email", AttributeValue.fromS(user.getEmail()));
+        item.put("password", AttributeValue.fromS(user.getPassword()));
+        item.put("isActive", AttributeValue.fromBool(user.isActive()));
+        item.put("activationCode", AttributeValue.fromS(user.getActivationCode()));
+
+        PutItemRequest request = PutItemRequest.builder()
+                .tableName(TABLE_NAME)
+                .item(item)
+                .build();
+
+        dynamoDbClient.putItem(request);
+    }
+
+    @Override
+    public Optional<User> findUserById(String userId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public void deleteUser(String userId) {
+
+    }
+
+    @Override
+    public List<User> findAllUsers() {
+        return List.of();
+    }
+}
